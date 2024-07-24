47f2d3ef6e8cec423032119d4ab4a4d82dab5df3
takecchi <takecchi.kobayashi@gmail.com>
2024-07-24 18:50:46 +0900

    select

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,18 @@
+'use client';
+
 import {MenuItem, OutlinedInput, Select} from "@mui/material";
+import {useState} from "react";
 
 export default function Home() {
+  const [value, setValue] = useState('');
   return (
     <main>
       <div style={{display: 'flex', padding: '20px', gap: '20px', flexDirection: 'column', maxWidth: '400px'}}>
         <OutlinedInput label={'TEST INPUT'}/>
-        <Select label={'TEST SELECT'}>
-          <MenuItem>ITEM1</MenuItem>
-          <MenuItem>ITEM2</MenuItem>
-          <MenuItem>ITEM3</MenuItem>
+        <Select value={value} label={'TEST SELECT'} onChange={(event)=>setValue(event.target.value)}>
+          <MenuItem value={1}>ITEM1</MenuItem>
+          <MenuItem value={2}>ITEM2</MenuItem>
+          <MenuItem value={3}>ITEM3</MenuItem>
         </Select>
       </div>
     </main>
